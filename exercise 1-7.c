448bccef8e0307e547ec9cb76c96e7faa27e39f6
poopoo <112705279+NoahBranley@users.noreply.github.com>
2023-10-03 11:38:58 +0800

    added 1-7

---
+++ exercise 1-7.c
@@ -0,0 +1,11 @@
+/*write a program to print the value of EOF*/
+#include <stdio.h>
+
+int main()
+{
+  int c;
+
+  while ((c = getchar()) != EOF)
+    putchar(EOF);
+}
+/*i think this is right lmao*/
